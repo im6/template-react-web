db123e96f1f3f50d8dce58c9bc404102311c9091
jinouwuque <ee2win@gmail.com>
2016-12-04 15:01:08 -0500

    remove redundency and test with react dipper

--- src/modules/layout/components/background/index.jsx
+++ src/modules/layout/components/background/index.jsx
@@ -1,23 +1,6 @@
 import React, { PropTypes } from 'react';
-import { Card } from 'antd';
-import drawCanvas from './particles.js';
+import ReactDipper from 'react-dipper';
 
-class Background extends React.Component {
-  constructor(props){
-    super(props);
-  }
-
-  componentDidMount(){
-    let me = this;
-    let { canvas } = me.refs;
-    let obj = me.props.param;
-    drawCanvas(canvas, obj);
-  }
-
-  render() {
-    let me = this;
-    return <canvas style={me.props.param.css} ref="canvas"/>
-  }
-};
+const Background = () => <ReactDipper />;
 
 export default Background;
