98913bb2b51a5d40f71e24c1e966a967dbf2123c
jinouwuque <ee2win@gmail.com>
2017-02-24 23:44:30 -0500

    enable grouped reducer hierarchy

--- src/config/reducer.js
+++ src/config/reducer.js
@@ -4,6 +4,9 @@ const keys = context.keys();
 export const moduleReducers = keys.reduce(function(res, v) {
   let str0 = v.replace(/\.js$/,'');
   let str1 = str0.replace(/^.\//,'');
+  if(str1.indexOf('/') > -1){
+    str1 = str1.replace(/\//, '_');
+  }
   res[str1] = context(v).default;
   return res;
 },{});
