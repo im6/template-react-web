1fe0be963624a3b83ef6805313e8f5a717ce7780
jinouwuque <ee2win@gmail.com>
2017-03-04 21:01:25 -0500

    update all the dependency to the latest

--- src/modules/layout/components/footer/index.jsx
+++ src/modules/layout/components/footer/index.jsx
@@ -5,8 +5,8 @@ import style from './style.less';
 const Footer = () =><footer className={style.footerContainer}>
   <div className={style.footerText}>
     <div className={style.frostedGlass}></div>
-    <h3>Built with react, react-router, redux, redux-thunk, ant-design, by ZJ Guo</h3>
+    <h3>Built with react, react-router, redux, redux-saga, ant-design</h3>
   </div>
-</footer>
+</footer>;
 
 export default Footer;
--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -4,14 +4,12 @@ import Immutable, {Map, List} from 'immutable';
 
 const todos = handleActions({
   ['todos/get'](state, action) {
-    console.log('loading todos...');
     return state.merge({
       loading: true,
       todoList: []
     });
   },
   ['todos/get/success'](state, action) {
-    console.log('loading todos success!');
     return state.merge({
       loading: false,
       todoList: action.payload.data
