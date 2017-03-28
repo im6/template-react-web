9da99f83fdd96312a90fa0d99c6e19e9eeee0fba
jinouwuque <ee2win@gmail.com>
2017-03-27 22:43:50 -0400

    add some auth before enter

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,15 +7,25 @@ import ErrorPage from '../modules/errorPage';
 import Hello from '../modules/hello';
 
 
-const Routes = ({ history, store }) =>
-  <Router history={history} >
-    <Route path="/" component={App}>
+const Routes = ({ history, store }) =>{
+  const checkAuth = (nextState, replace, callback) => {
+    if(nextState.location.pathname.length < 1){
+      replace('/error');
+    }
+    callback();
+  };
+
+  return <Router history={history} >
+    <Route path="/"
+           onEnter={checkAuth}
+           component={App}>
       <IndexRoute component={Hello}/>
       <Route path="todos" component={Todos}/>
       <Route path="users" component={Users} />
       <Route path="*" component={ErrorPage} />
     </Route>
   </Router>;
+};
 
 Routes.propTypes = {
   history: PropTypes.object.isRequired,
