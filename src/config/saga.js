dec8f45028e3782c3a1dae70b06b89bc3805c0fa
zj1926 <zj1926@users.noreply.github.com>
2017-05-17 14:25:02 -0400

    Update saga.js

--- src/config/saga.js
+++ src/config/saga.js
@@ -1,10 +1,9 @@
-
 const context = require.context('../sagas/', true, /\.js$/);
-let keys = context.keys();
-
-
-export const sagaInitiator = (sagaMiddleware) => {
-  keys.forEach(v =>{
+const keys = context.keys();
+const sagaInitiator = (sagaMiddleware) => {
+  keys.forEach((v) => {
     sagaMiddleware.run(context(v).default);
   });
 };
+
+export default sagaInitiator;
