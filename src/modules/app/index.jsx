a78601bd68f05d55ae52da24e41a674d114309d7
zj1926 <zj1926@users.noreply.github.com>
2017-05-17 14:26:48 -0400

    Update index.jsx

--- src/modules/app/index.jsx
+++ src/modules/app/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Layout from '../layout/index.jsx';
 import QueueAnim from 'rc-queue-anim';
+import Layout from '../layout/index.jsx';
 import { default as appService } from './service';
 
 class App extends React.Component {
@@ -10,14 +10,8 @@ class App extends React.Component {
     const me = this;
     me.state = {
       childrenCurrent: null,
-      isTransitionSlot: false
+      isTransitionSlot: false,
     };
-  }
-  shouldComponentUpdate(nextprops) {
-    const me = this;
-    const isSame = appService.compareRoutes(nextprops.routes, me.props.routes);
-    const willChange = !isSame || me.state.isTransitionSlot;
-    return willChange;
   }
 
   componentWillMount() {
@@ -28,13 +22,16 @@ class App extends React.Component {
   componentWillReceiveProps(nextProps) {
     const me = this;
     const isSame = appService.compareRoutes(nextProps.routes, me.props.routes);
-    if(isSame) {
-      return;
-    } else {
+    if (!isSame) {
       me.setDelayEffect();
     }
   }
-
+  shouldComponentUpdate(nextprops) {
+    const me = this;
+    const isSame = appService.compareRoutes(nextprops.routes, me.props.routes);
+    const willChange = !isSame || me.state.isTransitionSlot;
+    return willChange;
+  }
   setDelayEffect() {
     const me = this;
     me.setState({
@@ -50,17 +47,18 @@ class App extends React.Component {
   }
 
   render() {
-    let me = this;
+    const me = this;
     return (<Layout>
-      <QueueAnim delay={50}
-                 type={['right', 'left']}
-                 ease={['easeOutQuart', 'easeInOutQuart']}
-        >
+      <QueueAnim
+        delay={50}
+        type={['right', 'left']}
+        ease={['easeOutQuart', 'easeInOutQuart']}
+      >
         {
           me.state.childrenCurrent ?
-            <div key="a">
+            (<div key="a">
               { me.state.childrenCurrent }
-            </div>
+            </div>)
             : null
         }
       </QueueAnim>
@@ -70,6 +68,7 @@ class App extends React.Component {
 
 App.propTypes = {
   children: PropTypes.object.isRequired,
+  routes: PropTypes.array,
 };
 
 export default App;
