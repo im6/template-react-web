1a2451bec94567b3e62188ee57aeee519cfa7699
zj1926 <zj1926@users.noreply.github.com>
2017-05-17 14:24:45 -0400

    Update reducer.js

--- src/config/reducer.js
+++ src/config/reducer.js
@@ -1,13 +1,15 @@
+/* eslint no-param-reassign: 0*/
 const context = require.context('../reducers/', true, /\.js$/);
 const keys = context.keys();
 
-export const moduleReducers = keys.reduce((res, v) => {
-  let str0 = v.replace(/\.js$/,'');
-  let str1 = str0.replace(/^.\//,'');
-  if(str1.indexOf('/') > -1){
+const moduleReducers = keys.reduce((res, v) => {
+  const str0 = v.replace(/\.js$/, '');
+  let str1 = str0.replace(/^.\//, '');
+  if (str1.indexOf('/') > -1) {
     str1 = str1.replace(/\//, '_');
   }
   res[str1] = context(v).default;
   return res;
-},{});
+}, {});
 
+export default moduleReducers;
